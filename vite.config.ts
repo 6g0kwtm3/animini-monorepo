cb57e7eb9eabd9e96bc18ad279182ff76105a352
Patryk Wałach <35966385+PatrykWalach@users.noreply.github.com>
2023-10-29 22:26:32 +0100

    chore

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import tsconfigPaths from "vite-tsconfig-paths"
 
 export default defineConfig({
   plugins: [remix(config), tsconfigPaths()],
+  server:{
+    port:3000
+  },
   define: {
     "process.env.NODE_DEBUG": false,
   },
