2e0d97a3d4dd3a813418858e7f113d3632d0d4bb
Patryk Wałach <35966385+PatrykWalach@users.noreply.github.com>
2024-02-08 20:38:45 +0100

    fix: vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,22 @@
 import { paraglide } from "@inlang/paraglide-js-adapter-vite"
 import {
-	unstable_vitePlugin as remix,
-	unstable_cloudflarePreset as cloudflare
+	unstable_cloudflarePreset as cloudflare,
+	unstable_vitePlugin as remix
 } from "@remix-run/dev"
 import million from "million/compiler"
 import { remixDevTools } from "remix-development-tools/vite"
 import { defineConfig } from "vite"
 import tsconfigPaths from "vite-tsconfig-paths"
 
-import svgr from "vite-plugin-svgr"
 
 export default defineConfig({
 	plugins: [
-		svgr(),
 		paraglide({
 			project: "./project.inlang",
 			outdir: "./app/paraglide"
 		}),
 		remixDevTools(),
-		million.vite({ auto: true, mute: true }),
+		million.vite({ auto: true,log:false }),
 		remix({ ignoredRouteFiles: ["**/.*"], presets: [cloudflare()] }),
 		tsconfigPaths()
 	],
