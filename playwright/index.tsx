b1c1ccea121158346946050dcd7c61ea471e3f38
PatrykWalach <PatrykWalach@users.noreply.github.com>
2024-02-27 17:00:56 +0000

    refactor: run format

--- playwright/index.tsx
+++ playwright/index.tsx
@@ -1,12 +1,12 @@
 // Import styles, initialize component theme here.
 // import '../src/common.css';
-import '../app/tailwind.css';
+import "../app/tailwind.css"
 
-import { beforeMount } from '@playwright/experimental-ct-react/hooks';
+import { beforeMount } from "@playwright/experimental-ct-react/hooks"
 
 beforeMount(async ({ App }) => {
-  return (
-    <div className="theme-[#6751a4] bg-surface">
+	return (
+		<div className="bg-surface theme-[#6751a4]">
 			<App />
 		</div>
 	)
