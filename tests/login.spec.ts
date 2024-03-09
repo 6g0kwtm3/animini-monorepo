2a1e859122d4d2073e87ee79833bb6d4c1b6dee9
Patryk Wałach <35966385+PatrykWalach@users.noreply.github.com>
2024-03-09 20:12:26 +0100

    fix: hide vercel menu

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -2,6 +2,7 @@ import { test } from "@playwright/test"
 
 test("login", async ({ page }) => {
 	await page.goto("/")
+	await page.keyboard.press("Control+.")
 	await page.getByRole("link", { name: "Login" }).click()
 	await page.getByPlaceholder(" ").fill(process.env.ANILIST_TEST_TOKEN!)
 	await page.getByRole("button", { name: "Login" }).click()
