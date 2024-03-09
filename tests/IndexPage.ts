853862e0f06c4fdeaa7b93c6b61af89ccf282ec1
PatrykWalach <PatrykWalach@users.noreply.github.com>
2024-03-09 21:22:27 +0000

    refactor: run format

--- tests/IndexPage.ts
+++ tests/IndexPage.ts
@@ -1,15 +1,15 @@
-import type { Page } from "@playwright/test";
-import { expect } from "@playwright/test";
-import { Nav } from "./Nav";
+import type { Page } from "@playwright/test"
+import { expect } from "@playwright/test"
+import { Nav } from "./Nav"
 
 export class FeedPage {
-  nav: Nav;
-  private constructor(page: Page) {
-    this.nav = new Nav(page);
-  }
+	nav: Nav
+	private constructor(page: Page) {
+		this.nav = new Nav(page)
+	}
 
-  static async new(page: Page) {
-    await expect(page).toHaveTitle("Feed");
-    return new this(page);
-  }
+	static async new(page: Page) {
+		await expect(page).toHaveTitle("Feed")
+		return new this(page)
+	}
 }
