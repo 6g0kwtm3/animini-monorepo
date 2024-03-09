89bdee6fc483692349426b1fef41a59859ec2e44
PatrykWalach <PatrykWalach@users.noreply.github.com>
2024-03-09 17:51:23 +0000

    refactor: run format

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,10 +1,9 @@
-import { test } from '@playwright/test';
+import { test } from "@playwright/test"
 
-test('login', async ({ page }) => {
-  await page.goto('/');
-  await page.getByRole('link', { name: 'Login' }).click();
-  await page.getByPlaceholder(' ').fill(process.env.ANILIST_TEST_TOKEN!);
-  await page.getByRole('button', { name: 'Login' }).click();
-  await page.getByRole('link', { name: 'Profile' }).click();
-});
- 
+test("login", async ({ page }) => {
+	await page.goto("/")
+	await page.getByRole("link", { name: "Login" }).click()
+	await page.getByPlaceholder(" ").fill(process.env.ANILIST_TEST_TOKEN!)
+	await page.getByRole("button", { name: "Login" }).click()
+	await page.getByRole("link", { name: "Profile" }).click()
+})
