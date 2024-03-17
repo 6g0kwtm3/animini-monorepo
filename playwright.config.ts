b78c1ccc5c31a407e6b7cb1fbc834ec2139d6301
Patryk Wałach <35966385+PatrykWalach@users.noreply.github.com>
2024-03-17 11:31:23 +0100

    ci: test on action

--- playwright.config.ts
+++ playwright.config.ts
@@ -72,12 +72,12 @@ export default defineConfig({
 		//   name: 'Google Chrome',
 		//   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
 		// },
-	]
+	],
 
 	/* Run your local dev server before starting the tests */
-	// webServer: {
-	// 	command: "pnpm run build && pnpm run start",
-	// 	url: "http://localhost:3000",
-	// 	reuseExistingServer: !process.env.CI
-	// }
+	webServer: {
+		command: "pnpm run build && pnpm run start",
+		url: "http://localhost:3000",
+		reuseExistingServer: !process.env.CI
+	}
 })
