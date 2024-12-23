c4987cdda9fe63c9de0836bd90eb24cad777a1dc
Patryk Wałach <35966385+PatrykWalach@users.noreply.github.com>
2024-12-23 14:43:46 +0100

    fullyParallel

--- playwright.config.ts
+++ playwright.config.ts
@@ -14,9 +14,9 @@ dotenv.config()
 export default defineConfig({
 	testDir: "./tests",
 	timeout: 60000,
-	expect: { timeout: 10000 },
+	expect: { timeout: 60000 },
 	/* Run tests in files in parallel */
-	fullyParallel: false,
+	fullyParallel: true,
 	/* Fail the build on CI if you accidentally left test.only in the source code. */
 	forbidOnly: !!process.env.CI,
 	/* Retry on CI only */
