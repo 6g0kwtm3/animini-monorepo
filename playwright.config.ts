a00dc0ee9932a5357a67a71b07464f7e79780275
Patryk Wałach <35966385+PatrykWalach@users.noreply.github.com>
2024-12-23 16:00:01 +0100

    chore

--- playwright.config.ts
+++ playwright.config.ts
@@ -13,8 +13,6 @@ dotenv.config()
  */
 export default defineConfig({
 	testDir: "./tests",
-	timeout: 60000,
-	expect: { timeout: 60000 },
 	/* Run tests in files in parallel */
 	fullyParallel: true,
 	/* Fail the build on CI if you accidentally left test.only in the source code. */
