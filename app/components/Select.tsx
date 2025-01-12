1f749378bd54f0595ec17ef213fc2db77632cf83
Patryk Wałach <35966385+PatrykWalach@users.noreply.github.com>
2025-01-12 19:31:48 +0100

    remove lazySelect (#46)
    * remove lazySelect
    
    * refactor: run format
    
    * remove stuff
    
    ---------
    
    Co-authored-by: PatrykWalach <PatrykWalach@users.noreply.github.com>

--- app/components/Select.tsx
+++ app/components/Select.tsx
@@ -1,73 +1,56 @@
 import * as Ariakit from "@ariakit/react"
-import type { ComponentPropsWithoutRef, ReactNode } from "react"
-
+import type { ReactNode } from "react"
 import { createTextField } from "~/lib/textField"
 import { TextFieldOutlined } from "./TextField"
 
 // const onClient = Promise.resolve(null)
-import { Suspense, lazy } from "react"
+import { forwardRef } from "react"
+import { createMenu } from "~/lib/menu"
 
 const { input } = createTextField({})
 
-const LazySelectFactory = lazy(async () => import("./LazySelectFactory"))
-
-const LazySelect = lazy(async () => import("./LazySelect"))
 export function SelectFactory({
 	label,
-
 	...props
-}: (Omit<Ariakit.SelectProps, "ref"> & ComponentPropsWithoutRef<"select">) & {
+}: Ariakit.SelectProps & {
 	children: ReactNode
 	label: ReactNode
 	name: string
 }): ReactNode {
-	const form = Ariakit.useFormContext()
-	if (!form) throw new Error("FormSelect must be used within a Form")
-	// eslint-disable-next-line react-compiler/react-compiler
-	const value = form.useValue(props.name)
-
-	const fallback = (
-		<TextFieldOutlined>
-			<Ariakit.FormControl
-				name={props.name}
-				render={
-					<select
-						{...props}
-						value={value}
-						onChange={(e) => form.setValue(props.name, e.currentTarget.value)}
-						className={input({ className: "appearance-none" })}
-					/>
-				}
-			/>
-			<TextFieldOutlined.Label name={props.name}>
-				{label}
-			</TextFieldOutlined.Label>
-			<TextFieldOutlined.TrailingIcon className="pointer-events-none absolute right-0">
-				expand_more
-			</TextFieldOutlined.TrailingIcon>
-		</TextFieldOutlined>
-	)
-
 	return (
-		<Suspense fallback={fallback}>
-			<LazySelectFactory {...props} label={label} />
-		</Suspense>
+		<Ariakit.SelectProvider>
+			<TextFieldOutlined>
+				<Select {...props} />
+				<Ariakit.SelectLabel className="sr-only">{label}</Ariakit.SelectLabel>
+				<TextFieldOutlined.Label name={props.name}>
+					{label}
+				</TextFieldOutlined.Label>
+			</TextFieldOutlined>
+		</Ariakit.SelectProvider>
 	)
 }
 
-export function Select({
-	...props
-}: (Omit<Ariakit.SelectProps, "ref"> & ComponentPropsWithoutRef<"select">) & {
-	children: ReactNode
-	name: string
-}): ReactNode {
-	const fallback = (
-		<select {...props} className={input({ className: "appearance-none" })} />
-	)
+const { root } = createMenu()
 
-	return (
-		<Suspense fallback={fallback}>
-			<LazySelect {...props} />
-		</Suspense>
-	)
-}
+export const Select = forwardRef<HTMLButtonElement, Ariakit.SelectProps>(
+	function Select({ children, ...props }, ref) {
+		return (
+			<>
+				<Ariakit.Select
+					ref={ref}
+					{...props}
+					className={input({ className: "cursor-default" })}
+				/>
+				<Ariakit.SelectPopover
+					sameWidth
+					className={root({
+						className:
+							"z-10 max-h-[min(var(--popover-available-height,300px),300px)]",
+					})}
+				>
+					{children}
+				</Ariakit.SelectPopover>
+			</>
+		)
+	}
+)
