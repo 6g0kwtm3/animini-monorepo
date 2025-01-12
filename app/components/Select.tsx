7ca7c2bb95b3f985283db021a720e0d3d5960b5d
PatrykWalach <35966385+PatrykWalach@users.noreply.github.com>
2025-01-12 19:29:04 +0100

    remove stuff

--- app/components/Select.tsx
+++ app/components/Select.tsx
@@ -4,7 +4,7 @@ import { createTextField } from "~/lib/textField"
 import { TextFieldOutlined } from "./TextField"
 
 // const onClient = Promise.resolve(null)
-import { forwardRef, useRef } from "react"
+import { forwardRef } from "react"
 import { createMenu } from "~/lib/menu"
 
 const { input } = createTextField({})
@@ -32,50 +32,17 @@ export function SelectFactory({
 
 const { root } = createMenu()
 
-export interface SelectProps extends Ariakit.SelectProps {
-	value?: string
-	setValue?: (value: string) => void
-	defaultValue?: string
-	onBlur?: React.FocusEventHandler<HTMLElement>
-}
-
-import type { FocusEvent } from "react"
-
-export const Select = forwardRef<HTMLButtonElement, SelectProps>(
-	function LazySelect(
-		{ children, value, setValue, defaultValue, ...props },
-		ref
-	) {
-		const store = Ariakit.useSelectStore({
-			value,
-			setValue,
-			defaultValue: defaultValue ?? "",
-		})
-		const portalRef = useRef<HTMLElement>(null)
-
-		// Only call onBlur if the focus is leaving the whole widget.
-		const onBlur = (event: FocusEvent<HTMLElement>) => {
-			const portal = portalRef.current
-			const { selectElement, popoverElement } = store.getState()
-			if (portal?.contains(event.relatedTarget)) return
-			if (selectElement?.contains(event.relatedTarget)) return
-			if (popoverElement?.contains(event.relatedTarget)) return
-			props.onBlur?.(event)
-		}
-
+export const Select = forwardRef<HTMLButtonElement, Ariakit.SelectProps>(
+	function Select({ children, ...props }, ref) {
 		return (
 			<>
 				<Ariakit.Select
 					ref={ref}
 					{...props}
-					store={store}
-					onBlur={onBlur}
 					className={input({ className: "cursor-default" })}
 				/>
 				<Ariakit.SelectPopover
 					sameWidth
-					store={store}
-					onBlur={onBlur}
 					className={root({
 						className:
 							"z-10 max-h-[min(var(--popover-available-height,300px),300px)]",
