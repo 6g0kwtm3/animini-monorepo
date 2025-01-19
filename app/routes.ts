a49f95393934edf09be1ebe34b380f8182e7dfef
Patryk Wałach <35966385+PatrykWalach@users.noreply.github.com>
2025-01-19 20:40:24 +0100

    Remove flat routes (#50)
    * move routes
    
    * mv routes
    
    * define routes
    
    * refactor: run format
    
    * mv routes
    
    * update routes.ts
    
    * fix paths
    
    * fix routes
    
    ---------
    
    Co-authored-by: PatrykWalach <PatrykWalach@users.noreply.github.com>

--- app/routes.ts
+++ app/routes.ts
@@ -1,4 +1,25 @@
-import { type RouteConfig } from "@react-router/dev/routes"
-import { flatRoutes } from "@react-router/fs-routes"
+import {
+	index,
+	layout,
+	route,
+	type RouteConfig,
+} from "@react-router/dev/routes"
 
-export default flatRoutes() satisfies RouteConfig
+export default [
+	layout("./routes/Nav/route.tsx", [
+		index("./routes/Home/route.tsx"),
+		route("login", "./routes/Login/route.tsx"),
+		route("media/:mediaId", "./routes/Media/route.tsx", [
+			route("edit", "./routes/MediaEdit/route.tsx"),
+		]),
+		route("notifications", "./routes/Notifications/route.tsx"),
+		route("search", "./routes/Search/route.tsx"),
+		route("user/:userName", "./routes/User/route.tsx"),
+		route("user/:userName/:typelist", "./routes/UserList/route.tsx", [
+			route(":selected?", "./routes/UserListSelected/route.tsx"),
+		]),
+	]),
+	route("logout", "./routes/logout/route.tsx"),
+	route("user/:userId/follow", "./routes/UserFollow/route.tsx"),
+	route("user/:userId/info", "./routes/UserInfo/route.ts"),
+] satisfies RouteConfig
