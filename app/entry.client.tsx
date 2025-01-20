24e362e8b65ce64fe826298e8852bdcabedce17d
PatrykWalach <35966385+PatrykWalach@users.noreply.github.com>
2025-01-20 20:14:45 +0100

    add sentry

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -1,6 +1,25 @@
 import { HydratedRouter } from "react-router/dom"
 import { startTransition, StrictMode } from "react"
 import { hydrateRoot } from "react-dom/client"
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://examplePublicKey@o0.ingest.sentry.io/0",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration(),
+  ],
+
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+
+  // Set `tracePropagationTargets` to declare which URL(s) should have trace propagation enabled
+  tracePropagationTargets: [/^\//, /^https:\/\/yourserver\.io\/api/],
+
+  // Capture Replay for 10% of all sessions,
+  // plus 100% of sessions with an error
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+});
 
 startTransition(() => {
 	hydrateRoot(
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,29 @@
 import { paraglide } from "@inlang/paraglide-js-adapter-vite"
 import { reactRouter } from "@react-router/dev/vite"
+import { sentryVitePlugin } from "@sentry/vite-plugin"
 import icons from "unplugin-icons/vite"
 import { defineConfig } from "vite"
 import relay from "vite-plugin-relay"
 import tsconfigPaths from "vite-tsconfig-paths"
+import babel from "vite-plugin-babel"
 
 export default defineConfig({
 	plugins: [
+		babel({
+			babelConfig: {
+				presets: ["@babel/preset-typescript"],
+				plugins: [
+					["babel-plugin-relay"],
+					["babel-plugin-react-compiler", { target: "18" }],
+				],
+			},
+			filter: /\.[jt]sx?$/,
+			include: [
+				"app/routes/**/*.tsx",
+				"app/components/**/*.tsx",
+				"app/lib/**/*.tsx",
+			],
+		}),
 		// MillionLint.vite(),
 		paraglide({
 			project: "./project.inlang",
@@ -33,6 +50,10 @@ export default defineConfig({
 			},
 		}),
 		relay,
+		sentryVitePlugin({
+			org: "patryk-pi",
+			project: "javascript-react",
+		}),
 	],
 	server: {
 		port: 3000,
@@ -43,6 +64,9 @@ export default defineConfig({
 			`${Date.now()}` || process.env.NODE_ENV === "production"
 				? `${Date.now()}`
 				: "`${Date.now()}`",
+	},
+	build: {
+		sourcemap: true,
 	},
 })
 declare global {
