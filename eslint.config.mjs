b5b9110df69a4231f512c14a45f7693e6d6687ae
PatrykWalach <35966385+PatrykWalach@users.noreply.github.com>
2025-01-20 22:21:45 +0100

    fix errors

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,95 +1,60 @@
-import eslint from "@eslint/js"
-import * as graphql from "@graphql-eslint/eslint-plugin"
-
-import oxlint from "eslint-plugin-oxlint"
-import reactCompiler from "eslint-plugin-react-compiler"
-import reactRefresh from "eslint-plugin-react-refresh"
-import tseslint from "typescript-eslint"
-import graphQLConfig from "./graphql.config.js"
-
-export default tseslint.config(
-	{
-		ignores: [
-			"build",
-			"public/build",
-			"public/mockServiceWorker.js",
-			"**/schema.graphql",
-			"app/gql",
-            "playwright",
-			".react-router",
+/** @type {import('eslint').Linter.Config} */
+module.exports = {
+	extends: [
+		"plugin:compat/recommended",
+		"@remix-run/eslint-config",
+		"@remix-run/eslint-config/node",
+	],
+	plugins: ["react-refresh", "compat", "eslint-plugin-react-compiler"],
+	overrides: [
+		{
+			files: ["*.ts", "*.tsx"],
+			parserOptions: {
+				project: true,
+			},
+			rules: {
+				"react-compiler/react-compiler": "error",
+				"@typescript-eslint/no-unnecessary-condition": "error",
+				"@typescript-eslint/no-unnecessary-boolean-literal-compare": "error",
+				"@typescript-eslint/dot-notation": "error",
+				"@typescript-eslint/restrict-plus-operands": "warn",
+				"@typescript-eslint/no-floating-promises": "error",
+				"@typescript-eslint/no-misused-promises": "error",
+				"@typescript-eslint/return-await": "error",
+			},
+		},
+		{
+			files: ["*.js"],
+			processor: "@graphql-eslint/graphql",
+		},
+		{
+			files: ["*.graphql"],
+			extends: "plugin:@graphql-eslint/operations-all",
+		},
+	],
+	rules: {
+		"no-useless-constructor": "off",
+		"@typescript-eslint/no-unused-vars": ["warn"],
+		"@typescript-eslint/explicit-module-boundary-types": "warn",
+		"@typescript-eslint/method-signature-style": ["error", "property"],
+		"react/self-closing-comp": "error",
+		"react/jsx-no-useless-fragment": "warn",
+		"react-refresh/only-export-components": [
+			"error",
+			{
+				allowConstantExport: true,
+				allowExportNames: [
+					"meta",
+					"links",
+					"headers",
+					"loader",
+					"action",
+					"clientAction",
+					"clientLoader",
+					"config",
+					"shouldRevalidate",
+				],
+			},
 		],
 	},
-	// ...flat.extends(
-	// "plugin:compat/recommended",
-	// "@remix-run/eslint-config",
-	// "@remix-run/eslint-config/node"
-	// ),
-	eslint.configs.recommended,
-	tseslint.configs.base,
-
-	{
-		files: ["**/*.{js,jsx,ts,tsx}"],
-		plugins: {
-			"react-refresh": reactRefresh,
-			// compat: compat,
-			"react-compiler": reactCompiler,
-		},
-	},
-	{
-		files: ["**/*.{ts,tsx}"],
-		languageOptions: {
-			parserOptions: {
-				projectService: true,
-				tsconfigRootDir: import.meta.dirname,
-			},
-		},
-		rules: {
-			...tseslint.configs.recommended[2].rules,
-			// ...import_.configs.recommended.rules,
-			// ...import_.configs.typescript.rules,
-			"@typescript-eslint/no-unnecessary-condition": "error",
-			"@typescript-eslint/no-unnecessary-boolean-literal-compare": "error",
-			"@typescript-eslint/dot-notation": "error",
-			"@typescript-eslint/restrict-plus-operands": "warn",
-			"@typescript-eslint/no-floating-promises": "error",
-			// "@typescript-eslint/promise-function-async": "error",
-			"@typescript-eslint/no-misused-promises": "error",
-			"@typescript-eslint/return-await": "error",
-			"@typescript-eslint/no-unused-vars": ["warn"],
-			"@typescript-eslint/explicit-module-boundary-types": "warn",
-			"@typescript-eslint/method-signature-style": ["error", "property"],
-			"@typescript-eslint/no-explicit-any": "off",
-			"@typescript-eslint/no-empty-object-type": "off",
-		},
-	},
-	{
-		files: ["**/*.{js,jsx,ts,tsx}"],
-		processor: graphql.processors.graphql,
-		// languageOptions: { parser: tsparser },
-	},
-	{
-		files: ["**/*.graphql"],
-		languageOptions: {
-			parser: graphql.parser,
-			parserOptions: {
-				graphQLConfig: graphQLConfig,
-			},
-		},
-		plugins: {
-			"@graphql-eslint": { rules: graphql.rules },
-		},
-		rules: {
-			"@graphql-eslint/no-deprecated": "error",
-			"@graphql-eslint/no-duplicate-fields": "error",
-			"@graphql-eslint/require-selections": "error",
-			// "@graphql-eslint/selection-set-depth": ["warn", { maxDepth: 3 }],
-		},
-	},
-	{
-		rules: {
-			"no-undef": "off",
-			"no-unused-vars": "off",
-		},
-	},
-	...oxlint.buildFromOxlintConfigFile("./oxlintrc.json")
-)
+}
