66372eeb0879386ec74094e53c235faadf5bf027
Patryk Wałach <35966385+PatrykWalach@users.noreply.github.com>
2025-03-10 17:58:37 +0100

    fix typelist validation (#117)

--- app/lib/invariant.ts
+++ app/lib/invariant.ts
@@ -2,7 +2,9 @@ import { ArkErrors } from "arktype"
 
 export function invariant<T>(condition: T | ArkErrors): T {
 	if (condition instanceof ArkErrors) {
-		throw new Error(condition.summary)
+		throw new Response(condition.summary, {
+			status: 400,
+		})
 	}
 	return condition
 }
