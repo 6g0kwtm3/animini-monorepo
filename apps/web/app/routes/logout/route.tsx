c28655a3c4057139d71f40bd9289f870e8e721a2
Patryk Wałach <35966385+PatrykWalach@users.noreply.github.com>
2025-04-18 20:29:51 +0200

    invalidate store on auth (#210)

--- apps/web/app/routes/logout/route.tsx
+++ apps/web/app/routes/logout/route.tsx
@@ -1,6 +1,7 @@
 import { setUser } from "@sentry/react"
 import * as cookie from "cookie"
 import { redirect, type ClientLoaderFunctionArgs } from "react-router"
+import { commitLocalUpdate } from "~/lib/Network"
 
 export const clientAction = (args: ClientLoaderFunctionArgs) => {
 	const url = new URL(args.request.url)
@@ -12,6 +13,9 @@ export const clientAction = (args: ClientLoaderFunctionArgs) => {
 	})
 	document.cookie = setCookie
 	setUser(null)
+	commitLocalUpdate((store) => {
+		store.invalidateStore()
+	})
 	return redirect(url.searchParams.get("redirect") ?? "/", {
 		headers: {
 			"Set-Cookie": setCookie,
