2312b4f4befe6e0b189b0c82518b1c53d0c8f990
Patryk Wałach <35966385+PatrykWalach@users.noreply.github.com>
2025-04-21 17:35:25 +0200

    update eslint-plugin-relay LICENSE (#244)
    * add rule must-colocate-fragment-spreads
    
    * refactor: run format
    
    * fix lint errors
    
    * refactor: run format
    
    * fix lint errors
    
    * update LICENSE

--- packages/eslint-plugin-relay/src/rule-must-colocate-fragment-spreads.ts
+++ packages/eslint-plugin-relay/src/rule-must-colocate-fragment-spreads.ts
@@ -2,7 +2,7 @@
  * Copyright (c) Facebook, Inc. and its affiliates.
  *
  * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
+ * LICENSE file in the root directory of thttps://github.com/relayjs/eslint-plugin-relay.
  *
  * This rule lints for non-colocated fragment spreads within queries or
  * fragments. In other words, situations where a fragment is spread in module A,
