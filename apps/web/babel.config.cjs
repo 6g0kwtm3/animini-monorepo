f7308c8167bbfc2235dfbc686aae1f255f2af43c
Patryk Wałach <35966385+PatrykWalach@users.noreply.github.com>
2025-05-17 22:12:20 +0200

    fix babel tsx (#392)

--- apps/web/babel.config.cjs
+++ apps/web/babel.config.cjs
@@ -1,5 +1,10 @@
 module.exports = {
-	presets: ["@babel/preset-typescript"],
+	presets: [
+		[
+			"@babel/preset-typescript",
+			{ onlyRemoveTypeImports: true, allExtensions: true, isTSX: true },
+		],
+	],
 	babelrc: false,
 	plugins: [["babel-plugin-react-compiler", { target: "19" }]],
 }
