8e65df5eeeb54ac04d1bb306a9fee9d88d9e8526
PatrykWalach <35966385+PatrykWalach@users.noreply.github.com>
2025-05-30 21:46:05 +0200

    update storybook

--- apps/storybook/.storybook/main.ts
+++ apps/storybook/.storybook/main.ts
@@ -1,4 +1,8 @@
+import { createRequire } from "node:module";
+import { dirname, join } from "node:path";
 import type { StorybookConfig } from "@storybook/react-vite"
+
+const require = createRequire(import.meta.url);
 
 const config = {
 	stories: [
@@ -6,14 +10,17 @@ const config = {
 		"../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
 	],
 	addons: [
-		"@storybook/addon-onboarding",
-		{ name: "@storybook/addon-essentials", options: { backgrounds: false } },
-		"@chromatic-com/storybook",
-		"@storybook/addon-a11y",
-		"@storybook/addon-themes",
-		"@storybook/experimental-addon-test",
-	],
-	framework: { name: "@storybook/react-vite", options: {} },
+        getAbsolutePath("@storybook/addon-onboarding"),
+        getAbsolutePath("@chromatic-com/storybook"),
+        getAbsolutePath("@storybook/addon-a11y"),
+        getAbsolutePath("@storybook/addon-themes"),
+        getAbsolutePath("@storybook/addon-vitest")
+    ],
+	framework: { name: getAbsolutePath("@storybook/react-vite"), options: {} },
 } satisfies StorybookConfig
 
 export default config
+
+function getAbsolutePath(value: string): any {
+    return dirname(require.resolve(join(value, "package.json")));
+}
