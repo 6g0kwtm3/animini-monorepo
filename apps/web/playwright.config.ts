de856abcc17c5e3165dd558ca3344182f4d1370e
PatrykWalach <35966385+PatrykWalach@users.noreply.github.com>
2025-06-14 19:09:43 +0200

    use start --only

--- apps/web/playwright.config.ts
+++ apps/web/playwright.config.ts
@@ -76,7 +76,7 @@ export default defineConfig({
 
 	/* Run your local dev server before starting the tests */
 	webServer: {
-		command: "pnpm exec turbo web#start",
+		command: "pnpm exec turbo web#start --only",
 		url: "http://localhost:3000",
 		reuseExistingServer: !process.env.CI,
 	},
