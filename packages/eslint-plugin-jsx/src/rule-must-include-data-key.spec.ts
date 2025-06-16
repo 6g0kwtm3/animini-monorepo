fded85fa222294465b6a90bae8c92036e437b642
PatrykWalach <35966385+PatrykWalach@users.noreply.github.com>
2025-06-17 00:42:59 +0200

    lint data-key

---
+++ packages/eslint-plugin-jsx/src/rule-must-include-data-key.spec.ts
@@ -0,0 +1,125 @@
+import * as typescriptParser from "@typescript-eslint/parser"
+import { createRuleTester } from "eslint-vitest-rule-tester"
+import { describe } from "node:test"
+import { expect, test } from "vitest"
+import { rule } from "./rule-must-include-data-key"
+
+void describe("rule-must-include-data-key", () => {
+	const { valid, invalid } = createRuleTester({
+		rule,
+		languageOptions: {
+			ecmaVersion: 6,
+			parser: typescriptParser,
+			parserOptions: { ecmaFeatures: { jsx: true } },
+		},
+	})
+
+	test("allows equal key and data-key", async () => {
+		await valid(
+			`
+			<li key="foo" data-key="foo" />
+			`
+		)
+	})
+
+	test("allows equal key and data-key with boolean value", async () => {
+		await valid(
+			`
+			<li key data-key />
+			`
+		)
+	})
+
+	test("allows equal key and data-key with computed value", async () => {
+		await valid(
+			`
+      <li key={foo + bar} data-key={foo + bar} />
+      `
+		)
+	})
+
+	test("ignores whitespace", async () => {
+		await valid(
+			`
+      <li key={foo + bar} data-key={foo +\nbar} />
+      `
+		)
+	})
+
+	test("substitutes vars", async () => {
+		await valid(
+			`
+			const bar = "bar";
+      <li key="bar" data-key={bar} />
+      `
+		)
+	})
+
+	test("allows empty key and data-key", async () => {
+		await valid(
+			`
+      <li />
+      `
+		)
+	})
+
+	test("reports error for missing data-key", async () => {
+		const { result } = await invalid({
+			code: `
+        <li key="foo" />
+        `,
+			errors: ["must-include-data-key"],
+		})
+		expect(result.output).toMatchSnapshot()
+	})
+
+	test.skip("reports error for missing data-key with boolean value", async () => {
+		const { result } = await invalid({
+			code: `
+        <li key data-key="foo" />
+        `,
+			errors: ["data-key-must-match-key"],
+		})
+		expect(result.output).toMatchSnapshot()
+	})
+
+	test("reports error for different data-key", async () => {
+		const { result } = await invalid({
+			code: `
+        <li key={foo + bar} />
+        `,
+			errors: ["must-include-data-key"],
+		})
+		expect(result.output).toMatchSnapshot()
+	})
+
+	test("reports error for different data-key with computed value", async () => {
+		const { result } = await invalid({
+			code: `
+				<li key="foo" data-key={foo} />
+        `,
+			errors: ["data-key-must-match-key"],
+		})
+		expect(result.output).toMatchSnapshot()
+	})
+
+	test("reports error for missing data-key with computed value", async () => {
+		const { result } = await invalid({
+			code: `
+        <li key={foo + bar} data-key="foo" />
+        `,
+			errors: ["data-key-must-match-key"],
+		})
+		expect(result.output).toMatchSnapshot()
+	})
+
+	test("reports error for different data-key with computed value", async () => {
+		const { result } = await invalid({
+			code: `
+        <li key={foo + bar} data-key={foo} />
+        `,
+			errors: ["data-key-must-match-key"],
+		})
+		expect(result.output).toMatchSnapshot()
+	})
+})
---
+++ packages/eslint-plugin-jsx/src/rule-must-include-data-key.ts
@@ -0,0 +1,150 @@
+import {
+	type TSESTree,
+	AST_NODE_TYPES,
+} from "@typescript-eslint/typescript-estree"
+import { ASTUtils } from "@typescript-eslint/utils"
+import type { Rule } from "eslint"
+
+export const rule: Rule.RuleModule = {
+	meta: {
+		docs: {},
+		schema: [],
+		messages: {
+			"must-include-data-key":
+				"JSX element with 'key' prop must also have a matching 'data-key' prop.",
+			"data-key-must-match-key":
+				"The 'data-key' prop value must match the 'key' prop value.",
+		},
+		fixable: "code",
+	},
+	create(context) {
+		return {
+			JSXOpeningElement(node: TSESTree.JSXOpeningElement) {
+				const byName = Object.fromEntries(
+					node.attributes.flatMap(
+						(attribute): [string, TSESTree.JSXAttribute][] => {
+							switch (attribute.type) {
+								case AST_NODE_TYPES.JSXAttribute:
+									return [
+										[
+											(attribute.name.type === AST_NODE_TYPES.JSXIdentifier
+												? attribute.name
+												: attribute.name.name
+											).name,
+											attribute,
+										],
+									]
+								case AST_NODE_TYPES.JSXSpreadAttribute:
+									return []
+								default:
+									attribute satisfies never
+									throw new Error("unreachable")
+							}
+						}
+					)
+				)
+
+				const key = byName["key"]
+
+				if (!key || !key.value) {
+					return
+				}
+
+				const dataKey = byName["data-key"]
+
+				if (!dataKey) {
+					return context.report({
+						node,
+						messageId: "must-include-data-key",
+						data: {},
+						fix(fixer) {
+							return fixer.insertTextAfter(
+								key,
+								` data-key=${context.sourceCode.getText(key.value)}`
+							)
+						},
+					})
+				}
+
+				if (!isEqual(context, key.value, dataKey.value)) {
+					context.report({
+						node: dataKey,
+						messageId: "data-key-must-match-key",
+						data: {},
+						fix(fixer) {
+							return fixer.replaceText(
+								dataKey.value,
+								context.sourceCode.getText(key.value)
+							)
+						},
+					})
+				}
+			},
+		}
+	},
+}
+
+function isEqual(
+	context: Rule.RuleContext,
+	a: TSESTree.JSXElement | TSESTree.JSXExpression | TSESTree.Literal,
+	b: TSESTree.JSXElement | TSESTree.JSXExpression | TSESTree.Literal | null
+) {
+	if (a === b) {
+		return true
+	}
+
+	if (b === null) {
+		return false
+	}
+
+	if (
+		a.type === AST_NODE_TYPES.JSXElement
+		|| b.type === AST_NODE_TYPES.JSXElement
+	) {
+		return true
+	}
+
+	if ("expression" in a) {
+		a = a.expression
+	}
+	const evaluatedA = ASTUtils.getStaticValue(a, context.sourceCode.getScope(a))
+	if ("expression" in b) {
+		b = b.expression
+	}
+	const evaluatedB = ASTUtils.getStaticValue(b, context.sourceCode.getScope(b))
+
+	console.log(evaluatedA, evaluatedB)
+
+	if (evaluatedA && evaluatedB) {
+		return evaluatedA.value === evaluatedB.value
+	}
+
+	return stableStringify(context, a) === stableStringify(context, b)
+}
+
+function stableStringify(context: Rule.RuleContext, obj: {}) {
+	return JSON.stringify(obj, (key, value: unknown) => {
+		if (key === "loc" || key === "range" || key === "parent") {
+			return undefined
+		}
+
+		if (typeof value !== "object") {
+			return value
+		}
+
+		if (value == null) {
+			return value
+		}
+
+		if (Array.isArray(value)) {
+			return value
+		}
+
+		return (
+			ASTUtils.getStaticValue(value, context.sourceCode.getScope(value))
+			?? Object.fromEntries(
+				Object.entries(value).sort(([a], [b]) => a.localeCompare(b))
+			)
+		)
+	})
+}
