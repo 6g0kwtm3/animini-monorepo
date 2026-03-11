d31e947862a742a5c0e815210cef519cfd41c9b8
PatrykWalach <35966385+PatrykWalach@users.noreply.github.com>
2026-03-11 17:32:06 +0100

    feat(eslint): add react settings and enforce refresh rule

--- packages/eslint-config-react/src/eslint.config.ts
+++ packages/eslint-config-react/src/eslint.config.ts
@@ -11,13 +11,14 @@ import oxlintConfig from "oxlint-config" with { type: "json" }
 export default await typegen([
 	reactPlugin.configs.flat.recommended,
 	reactPlugin.configs.flat["jsx-runtime"],
-	{ settings: { react: { version: "19" } } },
 	reactHooks.configs.flat["recommended"],
 	reactRefresh.configs.vite,
 	jsx.configs.recommended,
 	{
-		name: "eslint-config-react/react-refresh/rules",
+		name: "eslint-config-react",
+		settings: { react: { version: "19" } },
 		rules: {
+			"react/jsx-no-leaked-render": "error",
 			"react-refresh/only-export-components": [
 				"error",
 				{
