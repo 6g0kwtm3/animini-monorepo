8b141afaec16b7a79796e77b97052d728e56a502
PatrykWalach <35966385+PatrykWalach@users.noreply.github.com>
2026-04-03 14:16:05 +0200

    refactor(tests simplify page object constructors and remove title checks feat(e2e): improve electron fixture video/trace handling chore(ci): remove debug file listing test workflow chorewright): change recording to only retain artifacts on failure chore(test): enable full Playwright traces/screenshots/videos (tests): remove unused and simplify async new() test(typelist replace TypelistPage checks with title : add debug step to list Playwright report and results refactor: run format fix(tests): retain or delete page instead unused attachVideo feat(tests): record attach videos/screens on fix(tests): use retry count for failed screenshot filename feat(tests): attach screenshot with options and filename using helper (tests): add tracing, video control & screenshot attach

--- apps/web/tests/fixtures.ts
+++ apps/web/tests/fixtures.ts
@@ -1,5 +1,11 @@
 import { defineNetworkFixture, type NetworkFixture } from "@msw/playwright"
-import base, { type ElectronApplication } from "@playwright/test"
+import base, {
+	type ElectronApplication,
+	type TestInfo,
+	type Video,
+	type VideoMode,
+	type ViewportSize,
+} from "@playwright/test"
 
 import { addMocksToSchema } from "@graphql-tools/mock"
 import { _electron } from "@playwright/test"
@@ -7,6 +13,7 @@ import fs from "fs"
 import { buildSchema, execute, parse } from "graphql"
 import { graphql, http, HttpResponse, type AnyHandler } from "msw"
 import { join } from "path"
+import { numberToString } from "~/lib/numberToString"
 
 function cached<T>(fn: () => T) {
 	let cache: T | undefined
@@ -46,6 +53,7 @@ export interface Fixtures extends Options {
 	handlers: AnyHandler[]
 	worker: NetworkFixture
 	electron: ElectronApplication | null
+	attachScreenshots: undefined
 }
 
 export const test = base.extend<Fixtures>({
@@ -69,22 +77,28 @@ export const test = base.extend<Fixtures>({
 
 	isElectron: [false, { option: true }],
 
-	async electron({ baseURL, isElectron }, provide) {
-		if (isElectron) {
-			const app = await _electron.launch({
-				args: ["."],
-				env: {
-					...process.env,
-					...(baseURL ? { EXISTING_SERVER_URL: baseURL } : {}),
-				},
-				// env: { HONO_PORT: String(5137 + testInfo.workerIndex) },
-			})
-
-			await provide(app)
-			await app.close()
+	async electron({ baseURL, isElectron, video }, provide, testInfo) {
+		if (!isElectron) {
+			await provide(null)
 			return
 		}
-		await provide(null)
+		const options =
+			typeof video === "string" ? { mode: video, size: undefined } : video
+		const app = await _electron.launch({
+			args: ["."],
+			env: {
+				...process.env,
+				...(baseURL ? { EXISTING_SERVER_URL: baseURL } : {}),
+			},
+			recordVideo:
+				options.mode === "retain-on-failure" || isMode(options.mode, testInfo)
+					? { dir: testInfo.outputDir, size: options.size }
+					: undefined,
+			// env: { HONO_PORT: String(5137 + testInfo.workerIndex) },
+		})
+
+		await provide(app)
+		await app.close()
 	},
 
 	async context({ context, electron }, provide) {
@@ -95,7 +109,7 @@ export const test = base.extend<Fixtures>({
 		await provide(electron.context())
 	},
 
-	async page({ context, electron }, provide) {
+	async page({ context, electron, video }, provide, testInfo) {
 		if (electron == null) {
 			const page = await context.newPage()
 			await page.goto("/")
@@ -106,6 +120,116 @@ export const test = base.extend<Fixtures>({
 
 		const page = await electron.firstWindow()
 		await provide(page)
+
 		await page.close()
+
+		const pageVideo = page.video()
+		if (!pageVideo) return
+		await retainOrDeleteVideo(pageVideo, video, testInfo)
 	},
+
+	attachScreenshots: [
+		async (
+			{ page, screenshot: screenshotOptions, isElectron },
+			provide,
+			testInfo
+		) => {
+			await provide(undefined)
+
+			if (!isElectron) {
+				return
+			}
+
+			const screenshot =
+				typeof screenshotOptions === "string"
+					? {
+							mode: screenshotOptions,
+							fullPage: undefined,
+							omitBackground: undefined,
+						}
+					: screenshotOptions
+
+			// After the test we can check whether the test passed or failed.
+			if (
+				screenshot.mode === "only-on-failure"
+					? isFailure(testInfo)
+					: screenshot.mode === "on-first-failure"
+						? isFirstFailure(testInfo)
+						: isMode(screenshot.mode, testInfo)
+			) {
+				const img = await page.screenshot({
+					fullPage: screenshot.fullPage,
+					omitBackground: screenshot.omitBackground,
+					path: testInfo.outputPath(
+						`test-failed-${numberToString(testInfo.retry + 1)}.png`
+					),
+				})
+				await testInfo.attach("screenshot", {
+					body: img,
+					contentType: "image/png",
+				})
+			}
+		},
+		{ auto: true },
+	],
 })
+
+function isFirstFailure(testInfo: TestInfo): boolean {
+	return isFailure(testInfo) && testInfo.retry === 0
+}
+
+function isFailure(testInfo: TestInfo): boolean {
+	return testInfo.status !== testInfo.expectedStatus
+}
+
+function isMode(
+	mode:
+		| "off"
+		| "on"
+		| "on-all-retries"
+		| "on-first-retry"
+		| "retry-with-trace"
+		| "retry-with-video",
+	testInfo: TestInfo
+): boolean {
+	switch (mode) {
+		case "off":
+			return false
+		case "on":
+			return true
+		case "on-all-retries":
+			return testInfo.retry > 0
+		case "on-first-retry":
+			return testInfo.retry === 1
+		case "retry-with-trace":
+			throw new Error('unknown mode "retry-with-trace"')
+		case "retry-with-video":
+			throw new Error('unknown mode "retry-with-video"')
+	}
+}
+
+async function retainOrDeleteVideo(
+	pageVideo: Video,
+	video:
+		| "retry-with-video"
+		| VideoMode
+		| { mode: VideoMode; size?: ViewportSize },
+	testInfo: TestInfo
+) {
+	const options =
+		typeof video === "string" ? { mode: video, size: undefined } : video
+
+	if (
+		options.mode === "retain-on-failure"
+			? isFailure(testInfo)
+			: isMode(options.mode, testInfo)
+	) {
+		await pageVideo.saveAs(testInfo.outputPath("video.webm"))
+		await testInfo.attach("video", {
+			contentType: "video/webm",
+			path: testInfo.outputPath("video.webm"),
+		})
+	} else {
+		await pageVideo.delete()
+	}
+}
