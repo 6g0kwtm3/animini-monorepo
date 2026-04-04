89d071c36aa2529594ffe1c5e675461ec7e66496
PatrykWalach <35966385+PatrykWalach@users.noreply.github.com>
2026-04-05 00:38:48 +0200

    feat: add Breadcrumb andItem components (route): add breadcrumb item and title outlet to UserList route feat(b): replace username link with Breadcrumb component (route): add title outlet and breadcrumb to UserListSelected

---
+++ apps/web/app/components/Breadcrumb.tsx
@@ -0,0 +1,45 @@
+import { A } from "@anitrove/a"
+import { utilities } from "@anitrove/design"
+import { precompileStyles, useStyles } from "@anitrove/unstyled"
+import { Role, type RoleProps } from "@ariakit/react"
+import type { ComponentProps } from "react"
+import {
+  useLocation,
+  useResolvedPath
+} from "react-router"
+
+export function Breadcrumb(props: RoleProps<"nav">) {
+	const [className, jsx] = useStyles(
+		precompileStyles({ ...utilities.lineClamp(1) })
+	)
+	return (
+		<Role.nav aria-label="Breadcrumb" {...props}>
+			<Role.ol className={className}>{props.children}</Role.ol>
+			{jsx}
+		</Role.nav>
+	)
+}
+
+export function BreadcrumbItem(props: ComponentProps<typeof A>) {
+	const [className, jsx] = useStyles(
+		precompileStyles({
+			...utilities.marginX({ "&:before": "0.25rem" }),
+			display: "inline-block",
+			content: { "&:first-child": { "&::before": "''" }, "&::before": "'/'" },
+		})
+	)
+
+	const { pathname } = useResolvedPath(props.href, { relative: props.relative })
+
+	const location = useLocation()
+
+	return (
+		<Role.li className={className}>
+			<A
+				aria-current={pathname === location.pathname ? "page" : undefined}
+				{...props}
+			></A>
+			{jsx}
+		</Role.li>
+	)
+}
--- apps/web/app/routes/UserListSelected/route.tsx
+++ apps/web/app/routes/UserListSelected/route.tsx
@@ -51,7 +51,8 @@ import type {
 
 import { captureException } from "@sentry/react"
 import { ArkErrors, type } from "arktype"
-import { ExtraOutlets } from "extra-outlet"
+import { ExtraOutlet, ExtraOutlets } from "extra-outlet"
+import { BreadcrumbItem } from "~/components/Breadcrumb"
 import type { routeFuzzyDateOrder_fuzzyDate$key as routeFuzzyDate$key } from "~/gql/routeFuzzyDateOrder_fuzzyDate.graphql"
 import type { routeUserSetStatusMutation } from "~/gql/routeUserSetStatusMutation.graphql"
 import { invariant } from "~/lib/invariant"
@@ -403,6 +404,14 @@ const Params = type({
 	typelist: '"animelist"|"mangalist"',
 })
 
+function Title({ params }: Route.ComponentProps): ReactNode {
+	return (
+		<>
+			<BreadcrumbItem href=".">{params.selected}</BreadcrumbItem>
+			<ExtraOutlet id="title" />
+		</>
+	)
+}
 export default function Page({
 	loaderData,
 	params,
@@ -411,7 +420,7 @@ export default function Page({
 	const [search] = useSearchParams()
 
 	return (
-		<ExtraOutlets>
+		<ExtraOutlets title={<Title {...props} />}>
 			{params.userName ? (
 				<title>
 					{params.typelist === "animelist"
