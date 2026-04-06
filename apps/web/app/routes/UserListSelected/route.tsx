185af7f2b5cffd182eb190b65bdcd4b2040132da
Patryk Wałach <35966385+PatrykWalach@users.noreply.github.com>
2026-04-06 22:23:33 +0200

    fix(route): conditionally renderItem when selected is set (#1581)
    * fix(route): conditionally renderItem when selected is set
    
    * refactor: run format
    
    ---------
    
    Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>

--- apps/web/app/routes/UserListSelected/route.tsx
+++ apps/web/app/routes/UserListSelected/route.tsx
@@ -407,7 +407,9 @@ const Params = type({
 function Title({ params }: Route.ComponentProps): ReactNode {
 	return (
 		<>
-			<BreadcrumbItem href=".">{params.selected}</BreadcrumbItem>
+			{params.selected ? (
+				<BreadcrumbItem href=".">{params.selected}</BreadcrumbItem>
+			) : null}
 			<ExtraOutlet id="title" />
 		</>
 	)
