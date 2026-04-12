1630de6838be5c9869db02556c62172d27f1c907
PatrykWalach <35966385+PatrykWalach@users.noreply.github.com>
2026-04-12 23:10:15 +0200

    feat(es): add types-eslint recommended config

--- packages/eslint-config/src/eslint.config.ts
+++ packages/eslint-config/src/eslint.config.ts
@@ -7,7 +7,7 @@ import turbo from "eslint-plugin-turbo"
 import typegen from "eslint-typegen"
 import oxlintConfig from "oxlint-config" with { type: "json" }
 import path from "path"
-
+import tseslint from "typescript-eslint"
 export default await typegen([
 	{
 		name: "eslint-config/ignores",
@@ -36,6 +36,7 @@ export default await typegen([
 			"perfectionist/sort-objects": "off",
 		},
 	},
+	...tseslint.configs.recommended,
 	{
 		name: "eslint-config/typescript-eslint/parser-options",
 		languageOptions: {
@@ -45,27 +46,6 @@ export default await typegen([
 			},
 		},
 	},
-	// {
-	// files: ["**/*.{ts,tsx}"],
-	// rules: {
-	// ...import_.configs.recommended.rules,
-	// ...import_.configs.typescript.rules,
-	// "@typescript-eslint/no-unnecessary-condition": "error",
-	// "@typescript-eslint/no-unnecessary-boolean-literal-compare": "error",
-	// "@typescript-eslint/dot-notation": "error",
-	// "@typescript-eslint/restrict-plus-operands": "warn",
-	// "@typescript-eslint/no-floating-promises": "error",
-	// "@typescript-eslint/promise-function-async": "error",
-	// "@typescript-eslint/no-misused-promises": "error",
-	// "@typescript-eslint/return-await": "error",
-	// "@typescript-eslint/no-unused-vars": ["warn"],
-	// "@typescript-eslint/explicit-module-boundary-types": "warn",
-	// "@typescript-eslint/method-signature-style": ["error", "property"],
-	// "@typescript-eslint/no-explicit-any": "off",
-	// 	},
-	// },
-
-	// turbo.configs["flat/recommended"],
 
 	{
 		rules: {
