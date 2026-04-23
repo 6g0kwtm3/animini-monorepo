6636e2a12c172882d98ca431638eef3edcacccaf
Patryk Wałach <35966385+PatrykWalach@users.noreply.github.com>
2026-04-23 23:06:35 +0200

    feat(media): add responsive src and sizes to MediaCover image (#1625)
    * feat(media): add responsive src and sizes to MediaCover image
    
    * fix(media): useToString for srcSet widthsFormatting

--- apps/web/app/lib/entry/MediaCover.tsx
+++ apps/web/app/lib/entry/MediaCover.tsx
@@ -13,6 +13,7 @@ const cover = tv({
 })
 
 import * as Ariakit from "@ariakit/react"
+import { numberToString } from "../numberToString"
 
 interface MediaCoverProps extends Ariakit.RoleProps<"img"> {
 	media: MediaCover_media$key
@@ -45,6 +46,19 @@ export function MediaCover({ media, ...props }: MediaCoverProps): ReactNode {
 				?? ""
 			}
 			loading="lazy"
+			sizes="auto"
+			srcSet={(
+				[
+					[100, data.coverImage?.medium],
+					[230, data.coverImage?.large],
+					[460, data.coverImage?.extraLarge],
+				] as const
+			)
+				.map(([width, url]) =>
+					url ? `${url} ${numberToString(width)}w` : null
+				)
+				.filter(Boolean)
+				.join(", ")}
 			alt=""
 			{...props}
 			style={{
