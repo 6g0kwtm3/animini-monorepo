e507fcdf24fd6bbec3bb9c34690d5fed6079769a
PatrykWalach <35966385+PatrykWalach@users.noreply.github.com>
2026-05-04 21:21:29 +0200

    test(login): Playwright login test that saves authMock successful auth and,
    fill token then press login,
    store session file.

---
+++ apps/web/tests/login.spec.ts
@@ -0,0 +1,17 @@
+import { SuccessHandler, test } from "./fixtures"
+import { FeedPage } from "./pages/IndexPage"
+
+const authFile = "playwright/.auth/user.json"
+
+test("authenticate", async ({ page, baseURL, worker }) => {
+	worker.use(SuccessHandler)
+	await page.goto(baseURL ?? "/")
+	await page.keyboard.press("Control+.")
+	const indexPage = await FeedPage.new(page)
+	const loginPage = await indexPage.nav.gotoLogin()
+	await loginPage.token.fill("Foo bar")
+	await loginPage.login.click()
+	await FeedPage.new(page)
+
+	await page.context().storageState({ path: authFile })
+})
