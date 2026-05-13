3087337deaf53f43969df8a22af4cdcef59b4b6d
PatrykWalach <35966385+PatrykWalach@users.noreply.github.com>
2026-05-13 23:23:22 +0200

    rsc

--- apps/web/react-router.config.ts
+++ apps/web/react-router.config.ts
@@ -3,11 +3,11 @@ import type { Config } from "@react-router/dev/config"
 export default {
 	future: {
 		unstable_optimizeDeps: true,
-		v8_splitRouteModules: true,
+		// v8_splitRouteModules: true,
 		v8_viteEnvironmentApi: true,
 		v8_middleware: true,
 	},
-	subResourceIntegrity: true,
+	// subResourceIntegrity: true,
 	ssr: false,
 	buildDirectory: "dist",
 } satisfies Config
